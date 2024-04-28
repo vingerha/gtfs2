cd4c13ade395b0aa14b67c1fb6d449a540c61cd5
Arjan <44190435+vingerha@users.noreply.github.com>
2024-04-28 15:39:38 +0200

    Update gtfs_rt_helper.py

--- custom_components/gtfs2/gtfs_rt_helper.py
+++ custom_components/gtfs2/gtfs_rt_helper.py
@@ -491,7 +491,7 @@ def convert_gtfs_realtime_alerts_to_json(gtfs_realtime_data):
             informed_entities = []
             for informed_entity in entity.alert.informed_entity:
                 informed_entity_json = {
-                    informed_entity: {
+                    "informed_entity": {
                         "agency_id": informed_entity.agency_id,
                         "route_id": informed_entity.route_id,
                         "trip_id": informed_entity.trip.trip_id
