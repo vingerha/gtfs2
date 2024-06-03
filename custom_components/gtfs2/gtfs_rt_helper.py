a71d37c14b74c569d1f068b212fb47dcd18ca57e
vingerha <44190435+vingerha@users.noreply.github.com>
2024-06-03 14:20:23 +0200

    Update gtfs_rt_helper.py

--- custom_components/gtfs2/gtfs_rt_helper.py
+++ custom_components/gtfs2/gtfs_rt_helper.py
@@ -277,17 +277,18 @@ def get_rt_vehicle_positions(self):
             # Vehicle is not in service
             continue
         if vehicle["trip"]["trip_id"] == self._trip_id: 
-            _LOGGER.debug('Adding position for TripId: %s, RouteId: %s, DirectionId: %s, Lat: %s, Lon: %s, crc_trip_id: %s', vehicle["trip"]["trip_id"],vehicle["trip"]["route_id"],vehicle["trip"]["direction_id"],vehicle["position"]["latitude"],vehicle["position"]["longitude"], binascii.crc32(str(vehicle["trip"]["trip_id"]).encode('utf8')))  
+            _LOGGER.debug('Adding position for TripId: %s, RouteId: %s, DirectionId: %s, Lat: %s, Lon: %s, crc_trip_id: %s', vehicle["trip"]["trip_id"],vehicle["trip"]["route_id"],vehicle["trip"]["direction_id"],vehicle["position"]["latitude"],vehicle["position"]["longitude"], binascii.crc32(str(vehicle["trip"]["trip_id"])))  
             
         # add data if in the selected direction
         if (str(self._route_id) == str(vehicle["trip"]["route_id"]) or str(vehicle["trip"]["trip_id"]) == str(self._trip_id)) and str(self._direction) == str(vehicle["trip"]["direction_id"]):
             _LOGGER.debug("Found vehicle on route with attributes: %s", vehicle)
+            _LOGGER.debug("crc : %s", binascii.crc32(str(vehicle["trip"]["trip_id"])) )
             geojson_element = {"geometry": {"coordinates":[],"type": "Point"}, "properties": {"id": "", "title": "", "trip_id": "", "route_id": "", "direction_id": "", "vehicle_id": "", "vehicle_label": ""}, "type": "Feature"}
             geojson_element["geometry"]["coordinates"] = []
             geojson_element["geometry"]["coordinates"].append(vehicle["position"]["longitude"])
             geojson_element["geometry"]["coordinates"].append(vehicle["position"]["latitude"])
-            geojson_element["properties"]["id"] = str(self._route_id) + "(" + str(vehicle["trip"]["direction_id"]) + ")" + binascii.crc32(str(vehicle["trip"]["trip_id"]).encode('utf8'))[-3:]            
-            geojson_element["properties"]["title"] = str(self._route_id) + "(" + str(vehicle["trip"]["direction_id"]) + ")" + binascii.crc32(str(vehicle["trip"]["trip_id"]).encode('utf8'))[-3:]
+            geojson_element["properties"]["id"] = str(self._route_id) + "(" + str(vehicle["trip"]["direction_id"]) + ")" + binascii.crc32(str(vehicle["trip"]["trip_id"]))[-3:]            
+            geojson_element["properties"]["title"] = str(self._route_id) + "(" + str(vehicle["trip"]["direction_id"]) + ")" + binascii.crc32(str(vehicle["trip"]["trip_id"]))[-3:]
             geojson_element["properties"]["trip_id"] = vehicle["trip"]["trip_id"]
             geojson_element["properties"]["route_id"] = str(self._route_id)
             geojson_element["properties"]["direction_id"] = vehicle["trip"]["direction_id"]
