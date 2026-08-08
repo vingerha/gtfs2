c56c840da287b061deccfef3d0a6a53cc1c133b2
Arjan <ajmvingerhoeds@hotmail.com>
2026-08-08 18:13:28 +0200

    Fix bug in vehicle positions
    When the start/stop route does not have a route_id then the vehicle realtime position mapping failed

--- custom_components/gtfs2/gtfs_rt_helper.py
+++ custom_components/gtfs2/gtfs_rt_helper.py
@@ -315,8 +315,12 @@ def get_rt_vehicle_positions(self):
         if vehicle["trip"]["trip_id"] == self._trip_id: 
             _LOGGER.debug('Adding position for TripId: %s, RouteId: %s, DirectionId: %s, Lat: %s, Lon: %s, crc_trip_id: %s', vehicle["trip"]["trip_id"],vehicle["trip"]["route_id"],vehicle["trip"]["direction_id"],vehicle["position"]["latitude"],vehicle["position"]["longitude"], binascii.crc32((vehicle["trip"]["trip_id"]).encode('utf8')))  
             
-        # add data if in the selected direction
-        if (str(self._route_id) == str(vehicle["trip"]["route_id"]) or str(vehicle["trip"]["trip_id"]) == str(self._trip_id)) and str(self._direction) == str(vehicle["trip"]["direction_id"]):
+        # add data if trip found or if route in the selected direction
+        if ( 
+            str(vehicle["trip"]["trip_id"]) == str(self._trip_id)
+            or 
+            ( str(self._route_id) == str(vehicle["trip"]["route_id"])  and str(self._direction) == str(vehicle["trip"]["direction_id"] ))
+            ):
             _LOGGER.debug("Found vehicle on route with attributes: %s", vehicle)
             _LOGGER.debug("crc : %s", binascii.crc32((vehicle["trip"]["trip_id"]).encode('utf8')))
             geojson_element = {"geometry": {"coordinates":[],"type": "Point"}, "properties": {"id": "", "title": "", "trip_id": "", "route_id": "", "direction_id": "", "vehicle_id": "", "vehicle_label": ""}, "type": "Feature"}
